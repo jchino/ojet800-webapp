0c4f2acaf4831e94cabd58308c9d5390c14f04f6
jchino <junko.chino@oracle.com>
2019-12-16 16:17:12 +0900

    Layout change, impl ojInvalidSelect event listner

--- src/js/viewModels/fileupload.js
+++ src/js/viewModels/fileupload.js
@@ -2,16 +2,16 @@
 
 define([
   'knockout',
+  'ojs/ojarraydataprovider',
   'ojs/ojlogger',
   'ojs/ojformlayout',
-  'ojs/ojlabel',
   'ojs/ojradioset',
   'ojs/ojfilepicker',
+  'ojs/ojmessages',
   'ojs/ojbutton',
 ],
-function (ko, Logger) {
-
-  Logger.option('level', Logger.LEVEL_INFO);
+// eslint-disable-next-line no-unused-vars
+function (ko, ArrayDataProvider, Logger) {
 
   const FileUploadViewModel = function () {
     // selected file(s)
@@ -23,6 +23,10 @@ function (ko, Logger) {
       }
       return names;
     }.bind(this));
+
+    // invalid file(s) message
+    this.invalidMsgs = ko.observableArray();
+    this.msgsProvider = new ArrayDataProvider(this.invalidMsgs); 
 
     // button's enable/disable control
     this.disabledUploadBtn = ko.observable(true);
@@ -42,25 +46,31 @@ function (ko, Logger) {
     // oj-file-picker property: selection-mode
     this.selectionMode = ko.observable('single');
 
+    // oj-file-picker event listener: ojInvalidSelect
+    this.invalidSelectAction = function (event) {
+      this.invalidMsgs(event.detail.messages);
+      if (event.detail.until) {
+        event.detail.until.then(
+          // ドラッグしていたマウスのポインタが外れたらメッセージをクリアする (Chrome, Firefox, Edge)
+          function () {
+            this.invalidMsgs.removeAll();
+          }.bind(this)
+        );
+      }
+    }.bind(this);
+
     // oj-file-picker event listner: ojBeforeSelect
     // eslint-disable-next-line no-unused-vars
     this.beforeSelectAction = function (event) {
+      // Safari は ojInvalidSelect イベントで設定されたメッセージがクリアされないことがあるので
+      // 念のためここでクリア
+      this.invalidMsgs.removeAll();
       // event.detail.accept(new Promise(function (resolve, reject) {
       //   // ユーザーが選択したファイルのアップロード前に実行したい非同期処理があればここに書く
       //   // 前処理が成功したら resolve()
-      //   // 前処理が失敗したら reject()
+      //   // 前処理が失敗したら reject() --> ojInvalidSelect イベントが発火されるが、event.detail.until は null
       // }));
-    };
-
-    // oj-file-picker event listener: ojInvalidSelect
-    this.invalidSelectAction = function (event) {
-      if (event.detail.until) {
-        // accept で指定した MIME タイプとは異なるファイルを選択した場合
-      }
-      else {
-        // ojBeforeSelect イベントのリスナーで reject された場合
-      }
-    };
+    }.bind(this);
 
     // oj-file-picker event listener: ojSelect
     this.selectAction = function (event) {
