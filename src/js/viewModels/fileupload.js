09dd2e25af35f1fa5daa9b5e627917a3ccc1f5f6
jchino <junko.chino@oracle.com>
2019-12-16 19:51:32 +0900

    impl file upload as binary

--- src/js/viewModels/fileupload.js
+++ src/js/viewModels/fileupload.js
@@ -9,6 +9,7 @@ define([
   'ojs/ojfilepicker',
   'ojs/ojmessages',
   'ojs/ojbutton',
+  'ojs/ojinputtext',
 ],
 // eslint-disable-next-line no-unused-vars
 function (ko, ArrayDataProvider, Logger) {
@@ -25,11 +26,16 @@ function (ko, ArrayDataProvider, Logger) {
     }.bind(this));
 
     // invalid file(s) message
-    this.invalidMsgs = ko.observableArray();
-    this.msgsProvider = new ArrayDataProvider(this.invalidMsgs); 
+    this.msgs = ko.observableArray();
+    this.msgsProvider = new ArrayDataProvider(this.msgs);
 
     // button's enable/disable control
     this.disabledUploadBtn = ko.observable(true);
+
+    // Upload Target Info
+    this.uploadUrl = ko.observable(); // アップロード先の URL
+    this.uploadHeader = ko.observable(); // アップロード時の Request Header
+    this.uploadBodyType = ko.observable('binary'); // アップロード時のリクエスト・タイプ
 
     // uplad status text
     this.uploadStatus = ko.observable('');
@@ -48,12 +54,12 @@ function (ko, ArrayDataProvider, Logger) {
 
     // oj-file-picker event listener: ojInvalidSelect
     this.invalidSelectAction = function (event) {
-      this.invalidMsgs(event.detail.messages);
+      this.msgs(event.detail.messages);
       if (event.detail.until) {
         event.detail.until.then(
           // ドラッグしていたマウスのポインタが外れたらメッセージをクリアする (Chrome, Firefox, Edge)
           function () {
-            this.invalidMsgs.removeAll();
+            this.msgs.removeAll();
           }.bind(this)
         );
       }
@@ -62,9 +68,8 @@ function (ko, ArrayDataProvider, Logger) {
     // oj-file-picker event listner: ojBeforeSelect
     // eslint-disable-next-line no-unused-vars
     this.beforeSelectAction = function (event) {
-      // Safari は ojInvalidSelect イベントで設定されたメッセージがクリアされないことがあるので
-      // 念のためここでクリア
-      this.invalidMsgs.removeAll();
+      // Safari は ojInvalidSelect イベントで設定したメッセージがクリアできないことがあるので念のためここでクリア
+      this.msgs.removeAll();
       // event.detail.accept(new Promise(function (resolve, reject) {
       //   // ユーザーが選択したファイルのアップロード前に実行したい非同期処理があればここに書く
       //   // 前処理が成功したら resolve()
@@ -86,36 +91,47 @@ function (ko, ArrayDataProvider, Logger) {
       this.uploadStatus('アップロード処理中');
       this.disabledUploadBtn(true);
       const promises = [];
-      for (let i = 0; i < this.selectedFiles().length; i++) {
-        promises.push(uploadFile(this.selectedFiles()[i]));
+      if (this.uploadBodyType() !== 'binary') {
+        alert('未実装です');
+        this.uploadStatus('');
       }
-      // Promise.all(promises).then(
-      //   function () {
-      //     this.uploadStatus('アップロード完了');
-      //     this.selectedFiles.removeAll();
-      //   }.bind(this),
-      //   function (error) {
-      //     Logger.error(error);
-      //   }
-      // );
+      else {
+        for (let i = 0; i < this.selectedFiles().length; i++) {
+          promises.push(uploadAsBinary(this.selectedFiles()[i]));
+        }
+        Promise.all(promises).then(
+          function () {
+            this.uploadStatus('アップロード完了');
+            this.selectedFiles.removeAll();
+          }.bind(this),
+          function (error) {
+            Logger.error(error);
+          }
+        );
+      }
+      
     }.bind(this);
 
-    // eslint-disable-next-line no-unused-vars
-    const uploadFile = function (file) {
-      // eslint-disable-next-line no-unused-vars
-      // return new Promise(function (resolve, reject) {
-      //   const xhr = new XMLHttpRequest();
-      //   xhr.open('POST', 'http://hostname:port/path/resource');
-      //   xhr.setRequestHeader('Authorization', 'Basic xxxxxxxx');
-      //   xhr.send(file);
-      //   xhr.onload = function () {
-      //     resolve();
-      //   };
-      //   xhr.onerror = function () {
-      //     reject();
-      //   };
-      // });
-    };
+    const uploadAsBinary = function (file) {
+      return new Promise(function (resolve) {
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', this.uploadUrl());
+        const headers = JSON.parse(this.uploadHeader());
+        const names = Object.keys(headers);
+        for (let i = 0; i < names.length; i++) {
+          xhr.setRequestHeader(names[i], headers[names[i]]);
+        }
+        xhr.send(file);
+        xhr.onload = function () {
+          this.msgs.push({ severity: 'info', summary: file.name + 'がアップロードされました' });
+          resolve();
+        }.bind(this);
+        xhr.onerror = function () {
+          this.msgs.push({ severity: 'error', summary: file.name + 'がアップロードできませんでした' });
+          resolve();
+        }.bind(this);
+      }.bind(this));
+    }.bind(this);
   };
 
   return FileUploadViewModel;
